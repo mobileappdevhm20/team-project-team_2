d34a2247d11830f5c84753c075c4d67ab48cac6b
Thomas <thomasreynaud74@outlook.com>
2020-05-19 15:43:13 +0200

    lobby_page

---
+++ app/src/main/java/com/mobileappdevelopment/themunichquiz/LobbyFragment.kt
@@ -0,0 +1,47 @@
+package com.mobileappdevelopment.themunichquiz
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_lobby.*
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class LobbyFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_lobby, container, false)
+
+//        button_settings.setOnClickListener {
+//
+//        }
+//
+//        button_addfriend.setOnClickListener {
+//
+//        }
+//
+//        button_playw_friend.setOnClickListener {
+//
+//        }
+//
+//        button_playw_random.setOnClickListener {
+//
+//        }
+//
+//        button_rules.setOnClickListener {
+//
+//        }
+//
+//        button_stats.setOnClickListener {
+//
+//        }
+    }
+
+}
