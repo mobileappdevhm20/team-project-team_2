bee59ed2571649a95e70ce19ea1c9e77158859bc
Thomas <thomasreynaud74@outlook.com>
2020-05-19 15:50:26 +0200

    lobby_page_

--- app/src/main/java/com/mobileappdevelopment/themunichquiz/LobbyFragment.kt
+++ app/src/main/java/com/mobileappdevelopment/themunichquiz/LobbyFragment.kt
@@ -7,16 +7,12 @@ import android.view.View
 import android.view.ViewGroup
 import kotlinx.android.synthetic.main.fragment_lobby.*
 
-/**
- * A simple [Fragment] subclass.
- */
 class LobbyFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_lobby, container, false)
 
 //        button_settings.setOnClickListener {
