e2bf0dd5efc9d02787b35cd63a37e2a45694cd00
fredrikholmberg <fredrik.np.holmberg@gmail.com>
2020-05-28 09:52:37 +0200

    Changed from PR

--- app/src/main/java/com/mobileappdevelopment/themunichquiz/SettingsFragment.kt
+++ app/src/main/java/com/mobileappdevelopment/themunichquiz/SettingsFragment.kt
@@ -1,2 +1,12 @@
 package com.mobileappdevelopment.themunichquiz
 
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SettingsFragment : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+    }
+}
