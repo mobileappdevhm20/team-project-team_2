84af93ec3654f6d6c423c2fe5a482efda19a4351
lukasf98 <lukasf98@users.noreply.github.com>
2020-06-08 12:15:08 +0200

    2 user auth (#44)
    * add log in
    
    * added sign up
    
    * added sign up
    
    Co-authored-by: hm-lfoerner <lfoerner@hm.edu>

--- app/src/main/java/com/mobileappdevelopment/themunichquiz/LoginFragment.kt
+++ app/src/main/java/com/mobileappdevelopment/themunichquiz/LoginFragment.kt
@@ -7,33 +7,47 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.Toast
+import androidx.annotation.NonNull
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.fragment_lobby.view.*
 import kotlinx.android.synthetic.main.login.*
 import kotlinx.android.synthetic.main.login.view.*
 
 class LoginFragment : Fragment() {
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.login, container, false)
 
+        auth = Firebase.auth
+
         view.button_login.setOnClickListener { view->
-            val username = text_username.text
-            val password = text_password.text
+            val username = text_username.text.toString()
+            val password = text_password.text.toString()
 
-            view.findNavController().navigate(R.id.action_loginFragment_to_lobbyFragment)
-
-            /**
             if(username.isEmpty() || password.isEmpty()) {
                 Toast.makeText(context, "Please enter username and password!", Toast.LENGTH_LONG).show()
             } else {
-                // TODO: Check if username and password is correct
-                // TODO: Navigate to lobby
+                auth.signInWithEmailAndPassword(username, password)
+                    .addOnCompleteListener(requireActivity()){task->
+                        if (task.isSuccessful) {
+                            view.findNavController().navigate(R.id.action_loginFragment_to_lobbyFragment)
+                        } else {
+                            Toast.makeText(context, "Authentication failed.",
+                                Toast.LENGTH_SHORT).show()
+                        }
+                    }
             }
-            **/
         }
 
         view.button_signup.setOnClickListener{ view ->
@@ -42,4 +56,6 @@ class LoginFragment : Fragment() {
 
         return view
     }
+
+
 }
