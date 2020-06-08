d33322f338ab304708cae0d334e1f296d3b532b9
hm-lfoerner <lfoerner@hm.edu>
2020-06-08 10:44:29 +0200

    added sign up

--- app/src/main/java/com/mobileappdevelopment/themunichquiz/SignUpFragment.kt
+++ app/src/main/java/com/mobileappdevelopment/themunichquiz/SignUpFragment.kt
@@ -5,11 +5,20 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.activity_signup.*
 import kotlinx.android.synthetic.main.activity_signup.view.*
+import kotlinx.android.synthetic.main.activity_signup.view.text_email
+import kotlinx.android.synthetic.main.activity_signup.view.text_password
+import kotlinx.android.synthetic.main.activity_signup.view.text_password_repeat
 
 class SignUpFragment : Fragment() {
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -17,9 +26,30 @@ class SignUpFragment : Fragment() {
     ): View? {
         val view = inflater.inflate(R.layout.activity_signup, container, false)
 
-        view.signupbtn.setOnClickListener { view ->
-            view.findNavController().navigate(R.id.action_signUpFragment2_to_loginFragment)
+        auth = Firebase.auth
+
+        view.button_signup.setOnClickListener { view ->
+            val email = text_email.text.toString()
+            val password = text_password.text.toString()
+            val passwordRepetition = text_password_repeat.text.toString()
+
+            if (!password.equals(passwordRepetition)) {
+                Toast.makeText(context, "Passwords are not equal!", Toast.LENGTH_LONG).show()
+            } else if (email.isEmpty() || password.isEmpty() || passwordRepetition.isEmpty()) {
+                Toast.makeText(context, "No field should be emtpy!", Toast.LENGTH_LONG).show()
+            }else {
+                auth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(requireActivity()) { task ->
+                        if (task.isSuccessful) {
+                            view.findNavController().navigate(R.id.action_signUpFragment2_to_loginFragment)
+                        } else {
+                            Toast.makeText(context, "Signup failed.",
+                                Toast.LENGTH_SHORT).show()
+                        }
+                    }
+            }
         }
+
         view.backbutton.setOnClickListener { view ->
             view.findNavController().navigate(R.id.action_signUpFragment2_to_loginFragment)
         }
